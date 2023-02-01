359884a979b112b72b8fa36ec783587f8dc689c6
mostafa <mustafa.bahr12323@gmail.com>
2023-02-01 15:06:56 +0200

    splash_view_animation

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,19 +1,72 @@
 import 'package:bookly_app/core/utils/assets_path.dart';
 import 'package:flutter/material.dart';
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({Key? key}) : super(key: key);
 
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with TickerProviderStateMixin  {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  late AnimationController animationController2;
+  late Animation<Offset> slidingAnimation2;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 2,),
+    );
+    slidingAnimation = Tween<Offset>(begin:const Offset(0 , 40) ,end: Offset.zero).animate(animationController);
+    animationController.forward();
+    // slidingAnimation.addListener(() {setState((){});});
+    animationController2 = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2,),
+    );
+    slidingAnimation2 = Tween<Offset>(begin:const Offset(0 , -5) ,end: Offset.zero).animate(animationController2);
+    animationController2.forward();
+    // slidingAnimation2.addListener(() {setState((){});});
+  }
+  @override
+  void dispose() {
+    super.dispose();
+    // اى controller لأازم نعمله dispose عشان ال resources
+    animationController.dispose();
+    animationController2.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return   Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Image.asset(AssetsPath.logo),
-        const SizedBox(height: 5,),
-        const Text("Read Free Books",
-        textAlign: TextAlign.center,
+        // بنعمل كدا عشان نحسن الاداء
+        AnimatedBuilder(
+          animation: slidingAnimation2,
+          builder: (context,_){
+            return  SlideTransition(
+                position: slidingAnimation2,
+                child: Image.asset(AssetsPath.logo));
+          },
+
         ),
+        const SizedBox(height: 5,),
+        AnimatedBuilder(
+          animation: slidingAnimation,
+          builder: (context,_){
+            return  SlideTransition(
+              position: slidingAnimation,
+              child:const Text("Read Free Books",
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
+
+        ),
+
       ],
     );
   }
