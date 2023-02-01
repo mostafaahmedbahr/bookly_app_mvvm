bceb9306f1b63bea139574679ad36046d1ad3483
mostafa <mustafa.bahr12323@gmail.com>
2023-02-02 00:43:52 +0200

    styles

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles
+{
+  static const titleMedium = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const titleLarge = TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.w900,
+  );
+}
