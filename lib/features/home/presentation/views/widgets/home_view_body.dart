8cc1ebe4bcd444a716216011700ea97f0a1a8f6f
mostafa <mustafa.bahr12323@gmail.com>
2023-02-02 00:49:37 +0200

    google fonts

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -23,6 +23,7 @@ class HomeViewBody extends StatelessWidget {
             style: AppStyles.titleMedium,
           ),
         ),
+
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'constants.dart';
 
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: primaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
       ),
       home:const SplashView(),
     );
